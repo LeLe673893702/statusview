bd63fc12440c1e9ed0ccfba2d2d379376a48e50b
LeLe673893702 <673893702@qq.com>
2020-05-20 08:43:20 +0800

    增加viewpager测试

--- app/src/main/java/com/newler/statusview/wrapactivity/ViewPagerActivity.kt
+++ app/src/main/java/com/newler/statusview/wrapactivity/ViewPagerActivity.kt
@@ -5,19 +5,27 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.newler.state.StateManager
 import com.newler.statusview.BaseActivity
 import com.newler.statusview.R
 import kotlinx.android.synthetic.main.activity_view_pager.*
+import kotlinx.android.synthetic.main.fragment_state.view.*
 
 class ViewPagerActivity : BaseActivity() {
     private val tabTitles by lazy {
         mutableListOf<String>("1", "2", "3", "4", "5")
     }
 
+    private val holder by lazy {
+        StateManager.instance.wrap(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_pager)
 
+        holder?.showEmpty()
+        holder?.showContent()
         vpState.adapter = TabPagAdapter(supportFragmentManager)
 
         tlState.setupWithViewPager(vpState)
